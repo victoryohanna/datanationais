8ea8ab4ff3ba81486f32fc4b2e0dac3414d715b6
Victor Yohanna <victoryohanna@gmail.com>
2023-07-02 22:40:28 +0100

    modified client component

--- src/components/clients.jsx
+++ src/components/clients.jsx
@@ -1,5 +1,7 @@
 
 import "../sass/clients.scss";
+import { client1, client2, client3 } from "./elements/services";
+
 const Clients = ()=>{
     return(
         <div className="clients-wrapper " id="clients-section">
@@ -10,7 +12,16 @@ const Clients = ()=>{
                 <div className="clients-section">
                     <div className="row">
                         <div className="col-md-3">
-                            <img src="" alt="" />
+                            <img src={client1} alt="" />
+                        </div>
+                        <div className="col-md-3">
+                            <img src={client2} alt="" />
+                        </div>
+                        <div className="col-md-3">
+                            <img src={client3} alt="" />
+                        </div>
+                        <div className="col-md-3">
+                            <img src={client2} alt="" />
                         </div>
                     </div>
                 </div>
---
+++ src/components/elements/services.js
@@ -0,0 +1,4 @@
+
+export {default as client1} from "../../assets/p_icon3.png";
+export {default as client2} from "../../assets/p_icon4.png";
+export {default as client3} from "../../assets/p_icon6.png";
