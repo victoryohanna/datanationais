b6fb569c351488e64eadf5a57c2d71248a167f15
Victor Yohanna <you@example.com>
2024-01-17 09:36:09 +0100

    add route paths

--- src/App.js
+++ src/App.js
@@ -7,14 +7,30 @@ import {
 } from "react-router-dom";
 import Navbar from "./components/navbar";
 import Home from "./pages/home";
+import Analytics from "./pages/analytics";
+import BusinessIntelligence from "./pages/businessIntelligence";
+import Consultancy from "./pages/consultancy";
+import Research from "./pages/research";
+import Training from "./pages/training";
+import WebDesign from "./pages/webdesign"
 import Footer from "./components/footer";
 
 export default function App() {
   const router = createBrowserRouter(
     createRoutesFromElements(
       <Route path="/" element={<Root />}>
-        <Route index element={<Home/>}/>
-      </Route>)
+        <Route index element={<Home />} />
+        <Route path="/data-analytics" element={<Analytics />} />
+        <Route
+          path="/business-intelligence"
+          element={<BusinessIntelligence />}
+        />
+        <Route path="/consultancy" element={<Consultancy />} />
+        <Route path="/research" element={<Research />} />
+        <Route path="/training" element={<Training />} />
+        <Route path="/web-design" element={<WebDesign />} />
+      </Route>
+    )
   );
 
   return (
