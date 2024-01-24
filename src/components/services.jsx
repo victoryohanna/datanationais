6cd794c44120f203d7aa31ddce55efbbe73dd98e
Victor Yohanna <you@example.com>
2024-01-24 13:40:03 +0100

    modified services section

--- src/components/services.jsx
+++ src/components/services.jsx
@@ -1,6 +1,6 @@
 import "../sass/services.scss";
 import { Link } from "react-router-dom";
-import RegisterCourse from "./register";
+
 
 const Services = () => {
   return (
@@ -9,103 +9,114 @@ const Services = () => {
         <div className="header-text">
           <h1>Our Services</h1>
           <span>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. epellat id
-            eos quos deleniti magni? Facilis, deserunt recusandae.
+            At DataNationIAS, we understand that navigating the complexities of
+            today's business landscape requires more than just expertise; we are here to provide 
+            strategic insight and innovation in the following fields:
           </span>
         </div>
         <div className="row">
           <div className="col-md-6">
             <div className="data-analytic">
-              <Link to="/data-analytics">
-                <h3>Data Analytics</h3>
-                <p>
-                  Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                  Eligendi, perspiciatis placeat consectetur deserunt quae quos
-                  voluptatibus ipsa ipsum error eum optio atque in veritatis
-                  voluptas fuga doloremque rem distinctio illo.
-                </p>
-              </Link>
+              {/* <Link to="/data-analytics"> */}
+              <h3>Data Analytics</h3>
+              <p>
+                Leverage the full potential of your data through our advanced
+                analytics and visualization services. From identifying trends to
+                uncovering hidden patterns, we turn complex datasets into clear,
+                insightful visuals that drive strategic decision-making.
+              </p>
+              <div className="d-flex justify-content-center">
+                <Link to="/">
+                  <h5>Read More...</h5>
+                </Link>
+              </div>
             </div>
           </div>
           <div className="col-md-6">
             <div className="research-section">
-              <Link to="/business-intelligence">
-                <h3>Business Intelligence</h3>
-                <p>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit
-                  error non quae alias veniam ullam quod asperiores dolor! Ipsum
-                  praesentium neque eius quam, itaque veniam illo cum vitae quo
-                  quae.
-                </p>
-              </Link>
+              {/* <Link to="/business-intelligence"> */}
+              <h3>Business Intelligence</h3>
+              <p>
+                Stay ahead of the curve with our predictive analytics services.
+                Our models utilize machine learning algorithms to forecast
+                future trends, helping you anticipate market shifts, customer
+                behavior, and operational needs.
+              </p>
+              <div className="d-flex justify-content-center">
+                <Link to="/">
+                  <h5>Read More...</h5>
+                </Link>
+              </div>
             </div>
           </div>
           <div className="col-md-6">
             <div className="research-section">
-              <Link to="/research">
-                <h3>Academic Research</h3>
-                <p>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit
-                  error non quae alias veniam ullam quod asperiores dolor! Ipsum
-                  praesentium neque eius quam, itaque veniam illo cum vitae quo
-                  quae.
-                </p>
-              </Link>
+              {/* <Link to="/research"> */}
+              <h3>Academic Research</h3>
+              <p>
+                We believe that the most profound discoveries often occur at the
+                intersections of different disciplines. Our researchers
+                collaborate across fields to approach complex issues from
+                multiple perspectives.
+              </p>
+              <div className="d-flex justify-content-center">
+                <Link to="/">
+                  <h5>Read More...</h5>
+                </Link>
+              </div>
             </div>
           </div>
           <div className="col-md-6">
             <div className="software_dev-section">
-              <Link to="/web-design">
-                <h3>Software Development</h3>
-                <p>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Eligendi blanditiis, repellat, dicta id veniam illo excepturi
-                  enim pariatur vel accusantium, voluptatum asperiores? Sit
-                  cupiditate voluptate quos aperiam voluptates. Cupiditate,
-                  voluptatum.
-                </p>
-              </Link>
+              {/* <Link to="/web-design"> */}
+              <h3>Software Development</h3>
+              <p>
+                Elevate your online presence with our web and mobile app
+                development expertise. We create responsive, intuitive, and
+                engaging applications that resonate with your target audience.
+              </p>
+              <div className="d-flex justify-content-center">
+                <Link to="/">
+                  <h5>Read More...</h5>
+                </Link>
+              </div>
             </div>
           </div>
           <div className="col-md-6">
             <div className="training-section">
-              <Link to="/training">
-                <h3>Training</h3>
-                <p>
-                  Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                  Numquam praesentium atque dignissimos ipsum temporibus
-                  voluptatum reiciendis fugit dicta sequi quam doloribus ab
-                  corporis, nulla perferendis est aut molestias unde quo.
-                </p>
-              </Link>
+              {/* <Link to="/training"> */}
+              <h3>Training</h3>
+              <p>
+                We are dedicated to pushing the boundaries of knowledge,
+                fostering intellectual curiosity, and addressing the critical
+                challenges of our time through rigorous training.
+              </p>
+              <div className="d-flex justify-content-center">
+                <Link to="/">
+                  <h5>Read More...</h5>
+                </Link>
+              </div>
             </div>
           </div>
           <div className="col-md-6">
             <div className="research-section">
-              <Link to="/consultancy">
-                <h3>Consultancy</h3>
-                <p>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit
-                  error non quae alias veniam ullam quod asperiores dolor! Ipsum
-                  praesentium neque eius quam, itaque veniam illo cum vitae quo
-                  quae.
-                </p>
-              </Link>
+              {/* <Link to="/consultancy"> */}
+              <h3>Consultancy</h3>
+              <p>
+                Unlock the full potential of your business with our strategic
+                consulting services. From market analysis to business planning,
+                our experts collaborate with you to develop tailored strategies
+                that drive growth and sustainability.
+              </p>
+              <div className="d-flex justify-content-center">
+                <Link to="/">
+                  <h5>Read More...</h5>
+                </Link>
+              </div>
             </div>
           </div>
-          {/* <div className="apply-button">
-            <button
-              type="button"
-              className="btn btn-apply"
-              data-bs-toggle="modal"
-              data-bs-target="#regModal"
-            >
-              Apply Now
-            </button>
-          </div> */}
         </div>
       </div>
-      <RegisterCourse />
     </div>
   );
 };
