7db6c0541a4b6eb0cab5ffcd395d915b15a1f0e1
Victor Yohanna <you@example.com>
2024-02-13 13:19:33 +0100

    modified components

--- src/components/services.jsx
+++ src/components/services.jsx
@@ -1,5 +1,10 @@
 import "../sass/services.scss";
 import { Link } from "react-router-dom";
+import { IoAnalytics } from "react-icons/io5";
+import { GiArtificialIntelligence, GiArchiveResearch } from "react-icons/gi";
+import { SiJirasoftware } from "react-icons/si";
+import { IoIosPeople } from "react-icons/io";
+import { FaHandshake } from "react-icons/fa";
 
 import RegisterCourse from "./common/register";
 
@@ -19,7 +24,14 @@ const Services = () => {
         <div className="row">
           <div className="col-md-6">
             <div className="data-analytic">
-              <h3>Data Analytics</h3>
+              <div className="caption">
+                <div>
+                  <div className="img-container">
+                    <IoAnalytics />
+                  </div>
+                </div>
+                <h4>Data Analytics</h4>
+              </div>
               <p>
                 Leverage the full potential of your data through our advanced
                 analytics and visualization services. From identifying trends to
@@ -35,7 +47,15 @@ const Services = () => {
           </div>
           <div className="col-md-6">
             <div className="research-section">
-              <h3>Business Intelligence</h3>
+              <div className="caption">
+                <div>
+                  <div className="img-container">
+                    <GiArtificialIntelligence />
+                  </div>
+                </div>
+                <h4>Business Intelligence</h4>
+              </div>
+
               <p>
                 Stay ahead of the curve with our predictive analytics services.
                 Our models utilize machine learning algorithms to forecast
@@ -51,7 +71,15 @@ const Services = () => {
           </div>
           <div className="col-md-6">
             <div className="research-section">
-              <h3>Academic Research</h3>
+              <div className="caption">
+                <div>
+                  <div className="img-container">
+                    <GiArchiveResearch />
+                  </div>
+                </div>
+                <h4>Academic Research</h4>
+              </div>
+
               <p>
                 We believe that the most profound discoveries often occur at the
                 intersections of different disciplines. Our researchers
@@ -67,7 +95,15 @@ const Services = () => {
           </div>
           <div className="col-md-6">
             <div className="software_dev-section">
-              <h3>Software Development</h3>
+              <div className="caption">
+                <div>
+                  <div className="img-container">
+                    <SiJirasoftware />
+                  </div>
+                </div>
+                <h4>Software Development</h4>
+              </div>
+
               <p>
                 Elevate your online presence with our web and mobile app
                 development expertise. We create responsive, intuitive, and
@@ -82,7 +118,15 @@ const Services = () => {
           </div>
           <div className="col-md-6">
             <div className="training-section">
-              <h3>Training</h3>
+              <div className="caption">
+                <div>
+                  <div className="img-container">
+                    <IoIosPeople />
+                  </div>
+                </div>
+                <h4>Training</h4>
+              </div>
+
               <p>
                 We are dedicated to pushing the boundaries of knowledge,
                 fostering intellectual curiosity, and addressing the critical
@@ -97,7 +141,15 @@ const Services = () => {
           </div>
           <div className="col-md-6">
             <div className="research-section">
-              <h3>Consultancy</h3>
+              <div className="caption">
+                <div>
+                  <div className="img-container">
+                    <FaHandshake />
+                  </div>
+                </div>
+                <h4>Consultancy</h4>
+              </div>
+
               <p>
                 Unlock the full potential of your business with our strategic
                 consulting services. From market analysis to business planning,
