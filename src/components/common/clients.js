4cb5df4f7d92c037721b36dfca74c45f4bf71c70
Victor Yohanna <you@example.com>
2024-02-15 03:56:49 +0100

    moved courses list to commons

---
+++ src/components/common/clients.js
@@ -0,0 +1,3 @@
+export { default as client1 } from "../../assets/p_icon3.png";
+export { default as client2 } from "../../assets/p_icon1.png";
+export { default as client3 } from "../../assets/p_icon2.jpeg";
