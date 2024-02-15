4ed1755bcb903c6aea29384cc18fd366e0385555
Victor Yohanna <you@example.com>
2024-02-15 04:23:31 +0100

    add page scroll custom hook

--- src/pages/services.jsx
+++ src/pages/services.jsx
@@ -2,13 +2,14 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 import NavLinks from "../components/nav";
 import Footer from "../components/footer";
+import ScrollToTop from "../components/elements/pageScroll";
 
 export default function Services() {
   return (
-    <>
+    <ScrollToTop>
       <NavLinks />
       <Outlet />
-      <Footer/>
-    </>
+      <Footer />
+    </ScrollToTop>
   );
 }
