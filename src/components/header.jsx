ca52c90643784fbc3bd1e671b1c0c8818801fdf7
Victor Yohanna <you@example.com>
2024-03-14 14:07:36 +0100

    src/components/header.jsx

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -6,19 +6,26 @@ import Banner from "./elements/Banner";
 const HeaderComponent = () => {
   return (
     <div className="container-header" id="hero-section">
-      
       <div
         className="carousel slide carousel-fade carousel-section "
         data-bs-ride="carousel"
       >
         <div className="carousel-inner">
           <div className="carousel-item active">
-            <img src={image1} className="img-carousel " alt="slide 1" /> 
+            <img src={image1} className="img-carousel " alt="slide 1" />
           </div>
+          {/* <script
+            src="https://static.elfsight.com/platform/platform.js"
+            data-use-service-core
+            defer
+          ></script>
+          <div
+            className="elfsight-app-53d34201-5890-4b38-85fd-f4b081f185f0"
+            data-elfsight-app-lazy
+          ></div> */}
           <div className="carousel-item">
             <img src={image2} className="img-carousel" alt="slide 2" />
           </div>
-          
         </div>
       </div>
       <Banner />
